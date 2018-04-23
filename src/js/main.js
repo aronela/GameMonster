abdcf98cd3acd707defb6981ff2807f748895f62
aronela <aronela.lucaci@emanuel.ro>
2018-04-23 08:48:21 +0300

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+
+const $ = require('jquery');
+
+global.$ = global.jQuery = $;
+
+require('../sass/style.scss');
+
+require('bootstrap/dist/css/bootstrap.css');
+
+require('bootstrap');
+
+
+
